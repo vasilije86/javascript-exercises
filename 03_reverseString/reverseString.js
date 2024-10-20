f0a9af2722947934607fa8fe4fc0358ef9b2ec1c
vasilije86 <vasilije.gto@gmail.com>
2024-10-20 22:25:38 +0200

    Completed 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let r = ""
+    while (string.length > 0) {
+        r += string[string.length -1]
+        string = string.substring(0, string.length -1)
+    }
+    return r
 };
 
 // Do not edit below this line
