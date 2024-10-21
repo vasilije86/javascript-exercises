d3de13fea58f29abcbbd1eaa07e8f92aaaf233bd
vasilije86 <vasilije.gto@gmail.com>
2024-10-21 13:12:58 +0200

    completed 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) / 1.8;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * 9/5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
