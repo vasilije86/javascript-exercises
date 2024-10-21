895493d7bd550e794a62895b5d6828379bbbab6b
vasilije86 <vasilije.gto@gmail.com>
2024-10-21 12:53:00 +0200

    Completed 04-05-06 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) return "ERROR";
+    if (n1 < 0 || n2 < 0) return "ERROR";
+    if (n1 > n2){
+        const helper = n2;
+        n2 = n1;
+        n1 = helper;
+    }
+    let sum =0;
+    for (i = n1;i<=n2;i++){
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
