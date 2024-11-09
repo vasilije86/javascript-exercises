3538fd41a5529e37b329b7949f9a742f937d299f
vasilije86 <vasilije.gto@gmail.com>
2024-11-10 00:44:13 +0100

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total,curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let r = 1;
+	for (let i = 0; i < b; i ++){
+    r *= a;
+  };
+  return r;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let r = 1;
+  for (a; a > 0; a--){
+    r *= a;
+  };
+  return r;
 };
 
 // Do not edit below this line
