9e6e54441d82011b8a61f0a6ca46b09fb2e85e19
vasilije86 <vasilije.gto@gmail.com>
2024-11-10 00:59:20 +0100

    Completed 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanum = 'qwertyuiopasdfghjklzxcvbnm1234567890';
+    const cleanString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanum.includes(char))
+        .join('');
+    const reversedString = cleanString.split('').reverse().join('');
 
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
