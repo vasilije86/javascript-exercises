0196577e21304767324c326b03c484d4b8b7969e
vasilije86 <vasilije.gto@gmail.com>
2024-11-10 01:32:23 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest,current) =>{
+        const currentYear = new Date().getFullYear();
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
