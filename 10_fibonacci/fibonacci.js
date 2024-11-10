b1d5ddea370b10cdaef4b7c7aab29c32d959d4d3
vasilije86 <vasilije.gto@gmail.com>
2024-11-10 01:08:13 +0100

    Completed 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    if (typeof(arg) === 'string') {
+        arg = parseInt(arg);
+    };
+    if (arg < 0) {
+        return 'OOPS';
+    };
+    if (arg === 1) {
+        return 1;
+    } else if (arg ===0){
+        return 0;
+    }else{
+        return fibonacci(arg - 1) + fibonacci (arg-2)
+    };
 };
 
 // Do not edit below this line
